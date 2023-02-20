e5ece7f235429b176b7038d116d81ca2c3f90a02
Josel099 <joseltech2002@gmail.com>
2023-02-21 00:15:23 +0530

    Basic heading and input field created

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component,text,submit } from 'react'
 
+import "./TodoApp.css"
 export default class TodoApp extends Component {
   render() {
     return (
-      <div>TodoApp</div>
+      <div class="todo-container">
+        <h1>To do </h1>
+        <div class="inputSession">
+            <input type={text}></input>
+            <button type={submit}> add</button>
+
+        </div>
+
+      </div>
     )
   }
 }
