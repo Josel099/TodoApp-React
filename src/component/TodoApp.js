6053603e51ab8d2534fe341b83eb36061419a653
Josel099 <joseltech2002@gmail.com>
2023-02-23 00:43:28 +0530

    error happend input filed disappered

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -2,13 +2,22 @@ import React, { Component,text } from 'react'
 
 import "./TodoApp.css"
 export default class TodoApp extends Component {
+  
+  handleChange = (event) =>{
+    this.setState({
+      input : event.target.value
+    });
+  }
+  
   render() {
+    
+    
     return (
       <div class="todo-container">
         <div className='todoAndInputField'>
         <h1>Todo App </h1>
         <form class="inputSession">
-            <input type={text} placeholder="enter the item's"></input>
+            <input type={text} value={input} onChange={this.handleChange} placeholder="enter the item's"></input>
             {/* <button type={submit}> add</button> */}
           
         </form>
