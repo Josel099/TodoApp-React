245aaaafb333130a9cca56a5d821bf4fb33b0a1d
Josel099 <joseltech2002@gmail.com>
2023-02-24 01:46:51 +0530

    completed

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -1,61 +1,68 @@
-import React, { Component,text } from 'react'
+import React, { Component, text } from "react";
 
-import "./TodoApp.css"
+import "./TodoApp.css";
 export default class TodoApp extends Component {
+  state = {
+    input: "",
+    items: [],
+  };
 
-      state ={
-        input: "",
-        items:[]
-      }
-  
- handleChange =event => {
-  this.setState({
-    input: event.target.value
- });
- //console.log(this.state.input);
- };
- 
-deleteItem=(index)=>{
-  const allItems =this.state.items;
-  allItems.splice(index,1);
-  this.setState({
-    items:allItems
-  })
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+    //console.log(this.state.input);
+  };
 
-}
+  deleteItem = (index) => {
+    const allItems = this.state.items;
+    allItems.splice(index, 1);
+    this.setState({
+      items: allItems,
+      //items:this.state.items.filter((data,index) => ! == index)
+    });
+  };
 
-
- storeItems =(event) =>{
-  event.preventDefault();
-  const {input } =this.state;
-  // const allItems =this.state.items;
-  // allItems.push(input);
-  this.setState({
-   items: [...this.state.items,input],
-   input:""
-  });
- }
+  storeItems = (event) => {
+    event.preventDefault();
+    const { input } = this.state;
+    // const allItems =this.state.items;
+    // allItems.push(input);
+    this.setState({
+      items: [...this.state.items, input],
+      input: "",
+    });
+  };
   render() {
-    const{input,items} =this.state;
-    
+    const { input, items } = this.state;
+
     return (
       <div className="todo-container">
-        <div className='todoAndInputField'>
-        <h1>Todo App </h1>
-        <form className="inputSession" onSubmit={this.storeItems}>
-            <input type={text} value={input} onChange={this.handleChange} placeholder="enter the item's"></input>
+        <div className="todoAndInputField">
+          <h1>Todo App </h1>
+          <form className="inputSession" onSubmit={this.storeItems}>
+            <input
+              type={text}
+              value={input}
+              onChange={this.handleChange}
+              placeholder="enter the item's"
+            ></input>
             {/* <button type={submit}> add</button> */}
-          
-        </form>
+          </form>
         </div>
         <ul>
-          {items.map((data,index) =>(
-              <li key={index}> {data}  <i className="fa-solid fa-trash" onClick={()=>this.deleteItem(index)}></i></li>
+          {items.map((data, index) => (
+            <li key={index}>
+              {" "}
+              {data}{" "}
+              <i
+                className="fa-solid fa-trash"
+                onClick={() => this.deleteItem(index)}
+              ></i>
+            </li>
           ))}
-              
-              
-            </ul>
+        </ul>
       </div>
-    )
+    );
   }
 }
