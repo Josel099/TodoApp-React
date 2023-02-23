d00ed8e4889bc33392d35acfcb04fc92f61218e4
Josel099 <joseltech2002@gmail.com>
2023-02-24 00:26:27 +0530

    the input is  stored in a temporary array

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -4,23 +4,34 @@ import "./TodoApp.css"
 export default class TodoApp extends Component {
 
       state ={
-        input: " "
+        input: " ",
+        items:[]
       }
   
  handleChange =event => {
   this.setState({
     input: event.target.value
  });
- console.log(this.state.input);
+ //console.log(this.state.input);
  };
  
+ storeItems =(event) =>{
+  event.preventDefault();
+  const {input } =this.state;
+  // const allItems =this.state.items;
+  // allItems.push(input);
+  this.setState({
+    items: [...this.state.items,input]
+  });
+ }
   render() {
-    const{input} =this.state;
+    const{input,items} =this.state;
+    console.log(items);
     return (
       <div className="todo-container">
         <div className='todoAndInputField'>
         <h1>Todo App </h1>
-        <form className="inputSession">
+        <form className="inputSession" onSubmit={this.storeItems}>
             <input type={text} vlaue={input} onChange={this.handleChange} placeholder="enter the item's"></input>
             {/* <button type={submit}> add</button> */}
           
@@ -28,10 +39,7 @@ export default class TodoApp extends Component {
         </div>
         <ul>
                 <li > hey  <i className="fa-solid fa-trash"></i></li>
-                <li> hey <i className="fa-solid fa-trash"></i></li>
-                <li> hey <i className="fa-solid fa-trash"></i></li>
-                <li> hey <i className="fa-solid fa-trash"></i></li>
-
+              
             </ul>
       </div>
     )
