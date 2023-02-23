0a02f2bf8636233cc53c57d920da61f22ce91e8f
Josel099 <joseltech2002@gmail.com>
2023-02-24 01:13:52 +0530

    when the value entered it displayed below as a list

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -4,7 +4,7 @@ import "./TodoApp.css"
 export default class TodoApp extends Component {
 
       state ={
-        input: " ",
+        input: "",
         items:[]
       }
   
@@ -15,30 +15,39 @@ export default class TodoApp extends Component {
  //console.log(this.state.input);
  };
  
+deleteItem=()=>{
+  
+}
+
+
  storeItems =(event) =>{
   event.preventDefault();
   const {input } =this.state;
   // const allItems =this.state.items;
   // allItems.push(input);
   this.setState({
-    items: [...this.state.items,input]
+   items: [...this.state.items,input],
+   input:""
   });
  }
   render() {
     const{input,items} =this.state;
-    console.log(items);
+    
     return (
       <div className="todo-container">
         <div className='todoAndInputField'>
         <h1>Todo App </h1>
         <form className="inputSession" onSubmit={this.storeItems}>
-            <input type={text} vlaue={input} onChange={this.handleChange} placeholder="enter the item's"></input>
+            <input type={text} value={input} onChange={this.handleChange} placeholder="enter the item's"></input>
             {/* <button type={submit}> add</button> */}
           
         </form>
         </div>
         <ul>
-                <li > hey  <i className="fa-solid fa-trash"></i></li>
+          {items.map((data,index) =>(
+              <li key={index}> {data}  <i className="fa-solid fa-trash" onClick={()=>this.deleteItem(index)}></i></li>
+          ))}
+              
               
             </ul>
       </div>
