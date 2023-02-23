87452f451bdd335a9c5fb3be539ee45bf279dbd8
Josel099 <joseltech2002@gmail.com>
2023-02-23 23:56:40 +0530

    input value stored to a state-- commited to master2

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -2,22 +2,35 @@ import React, { Component,text } from 'react'
 
 import "./TodoApp.css"
 export default class TodoApp extends Component {
+
+      state ={
+        input: " "
+      }
+  
+ handleChange =event => {
+  this.setState({
+    input: event.target.value
+ });
+ console.log(this.state.input);
+ };
+ 
   render() {
+    const{input} =this.state;
     return (
-      <div class="todo-container">
+      <div className="todo-container">
         <div className='todoAndInputField'>
         <h1>Todo App </h1>
-        <form class="inputSession">
-            <input type={text} placeholder="enter the item's"></input>
+        <form className="inputSession">
+            <input type={text} vlaue={input} onChange={this.handleChange} placeholder="enter the item's"></input>
             {/* <button type={submit}> add</button> */}
           
         </form>
         </div>
         <ul>
-                <li > hey  <i class="fa-solid fa-trash"></i></li>
-                <li> hey <i class="fa-solid fa-trash"></i></li>
-                <li> hey <i class="fa-solid fa-trash"></i></li>
-                <li> hey <i class="fa-solid fa-trash"></i></li>
+                <li > hey  <i className="fa-solid fa-trash"></i></li>
+                <li> hey <i className="fa-solid fa-trash"></i></li>
+                <li> hey <i className="fa-solid fa-trash"></i></li>
+                <li> hey <i className="fa-solid fa-trash"></i></li>
 
             </ul>
       </div>
